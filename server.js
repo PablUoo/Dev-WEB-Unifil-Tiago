afff656a26c0d239c4bf5620840a783d0a532058
pablo <pabloaurelio1163@gmail.com>
2024-08-09 22:16:30 -0300

    fix cepRegex

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
+const cepRegex = /^[0-9]{5}-?\[0-9]{3}$/;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -15,14 +17,18 @@ app.get('/index', (req, res) => {
 app.get('/consulta-cep/:cep', async (req, res) => {
     const cep = req.params.cep;
 
-    try {
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Erro na consulta da requisição Cep', error);
-        res.status(500).send("Erro ao consultar Cep");
-    }
-});
+        if(cepRegex.test(cep)){
+            try {
+                const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+                res.json(response.data);
+            } catch (error) {
+                console.error('Erro na consulta da requisição Cep', error);
+                res.status(500).send("Erro ao consultar Cep");
+            }
+        }else{
+            res.status(400).send("Cep Invalido");
+        }
+    });
 
 app.get('/consulta-moeda/:moeda/:numero_dias', async (req, res) => {
     const moeda = req.params.moeda;
